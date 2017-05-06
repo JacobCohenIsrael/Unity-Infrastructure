b68e4a2e61a65f20b28f81eba6072fb52bbdcf3a
Eyal Gold <Eyalgoldd@gmail.com>
2017-05-06 16:55:04 +0300

    testing first commit

--- Assets/Scripts/Infrastructure/Core/Login/Events/LoginSuccessfulEvent.cs
+++ Assets/Scripts/Infrastructure/Core/Login/Events/LoginSuccessfulEvent.cs
@@ -1,14 +1,20 @@
 ﻿using Infrastructure.Core.Player;
+using Infrastructure.Core.Star;
+using System.Collections.Generic;
+
 namespace Infrastructure.Core.Login.Events
 {
     public class LoginSuccessfulEvent : Infrastructure.Base.Event.Event
     {
         public PlayerModel player;
 
-        public LoginSuccessfulEvent(PlayerModel player)
+        public Dictionary<string, StarModel> starsList;
+
+        public LoginSuccessfulEvent(PlayerModel player, Dictionary<string, StarModel> starsList)
         {
             this.player = player;
-        }
+            this.starsList = starsList;
+        }   
     }
 }
 
