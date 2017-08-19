5d88271427de43e4cd46c9385eda9c26e5f3c2f1
Eyal Gold <Eyalgoldd@gmail.com>
2017-08-19 21:23:52 +0300

    adding playerEnterMarket there is an issue wit the buy/sell.

---
+++ Assets/Scripts/Infrastructure/Core/Player/Events/PlayerEnteredMarketEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Infrastructure.Core.Resource;
+using Infrastructure.Core.Star;
+
+namespace Infrastructure.Core.Player.Events
+{
+    public class PlayerEnteredMarketEvent : Infrastructure.Base.Event.Event
+    {
+        public PlayerModel Player;
+        public Dictionary<string, ResourceSlotModel> ResourceSlotList;
+    }
+}
+
